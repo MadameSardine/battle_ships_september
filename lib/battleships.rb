38a9206f2f3476eea90dbf767326a673b7b8645a
Sandrine <sandrine.zhangferron@gmail.com>
2014-09-29 17:40:47 +0100

    Added new game link and name form

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -1,8 +1,15 @@
 require 'sinatra/base'
 
 class BattleShips < Sinatra::Base
+	set :views, settings.root + '/../views/'
   get '/' do
     'Hello BattleShips!'
+    erb :index
+  end
+
+  get '/new_game' do
+  	@player1 = params[:name]
+  	erb :new_game
   end
 
   # start the server if ruby file executed directly
