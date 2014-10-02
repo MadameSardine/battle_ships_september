3213d828dd9ba3e7897523579f6b0b2b55fd0225
Sandrine <sandrine.zhangferron@gmail.com>
2014-10-02 19:45:48 +0100

    Testing that ship placing and shots work on boards

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -1,15 +1,26 @@
 require 'sinatra/base'
 require_relative 'game'
 require_relative 'player'
+require_relative 'board'
+require_relative 'cell'
+require_relative 'water'
+require_relative 'ship'
 
 class BattleShips < Sinatra::Base
 	set :views, settings.root + '/../views/'
   set :public_folder, settings.root + '/../public/'
   enable :sessions
 
-  GAME = Game.new
+  GAME = Game.new  
+  BOARD1 = Board.new(Cell)
+  BOARD2 = Board.new(Cell)
+  SHIT = Ship.battleship
+
+
+
 
   get '/' do
+    puts session.inspect
     @player1 = GAME.player1.name unless GAME.player1.nil?
     @player2 = GAME.player2.name unless GAME.player2.nil?
     @name = session[:me]
@@ -17,11 +28,11 @@ class BattleShips < Sinatra::Base
   end
 
   get '/new_game' do
-  	@name = session[:something]
   	erb :new_game
   end
 
   post'/new_player' do
+    puts session.inspect
     player = Player.new
     player.name =params[:player_name]
     session[:me] = params[:player_name]
@@ -29,6 +40,15 @@ class BattleShips < Sinatra::Base
     puts player.inspect
     puts GAME.inspect
     redirect '/'
+  end
+
+  get '/grid' do
+    BOARD1.place(SHIT, :D5) 
+   BOARD1.shoot_at(:A1) 
+    BOARD1.shoot_at(:D5)
+
+   
+    erb :grid
   end
 
   # start the server if ruby file executed directly
